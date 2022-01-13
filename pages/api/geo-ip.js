29c5c17ae9f75c72faad900bd294f3608e38a9cd
HanSeong Lee <jkhanseong@gmail.com>
2022-01-13 23:03:37 +0900

    ADD | Domain parameter of Geo ip API

--- pages/api/geo-ip.js
+++ pages/api/geo-ip.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
-        const {ip} = req.query;
-        const {data} = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.GEO_IPIFY_API_KEY}&ipAddress=${ip}`);
+        const {ip: ipAddress, domain} = req.query;
+        const {data} = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.GEO_IPIFY_API_KEY}`, {
+            params: {
+                ipAddress,
+                domain,
+            }
+        });
         return res.status(200).json(data);
     }
     throw new Error(
