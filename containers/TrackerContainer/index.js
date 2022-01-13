f551da55523af72396e3c7b11998f1c65cd3a2d3
HanSeong Lee <jkhanseong@gmail.com>
2022-01-13 23:42:20 +0900

    ADD | Own IP Address on the initial page load

--- containers/TrackerContainer/index.js
+++ containers/TrackerContainer/index.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Tracker from "../../components/Tracker";
 import defaultData from '../../data/data';
 import axios from "axios";
+import {getGeoIP} from "../../libs/apis/geo-ip";
 
 const TrackerContainer = () => {
     const [data, setData] = useState(defaultData);
@@ -9,28 +10,31 @@ const TrackerContainer = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
         const address = e.target[0].value;
-        console.log(address);
         const checkIP = /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/g;
         if (checkIP.test(address)) {
-            const {data} = await axios.get('/api/geo-ip', {
-                params: {
-                    ip: address,
-                }
+            const data = await getGeoIP({
+                ip: address,
             });
             setData(data);
             return;
         }
         const checkDomain = /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/g;
         if (checkDomain.test(address)) {
-            const {data} = await axios.get('/api/geo-ip', {
-                params: {
-                    domain: address,
-                }
+            const data = await getGeoIP({
+                domain: address,
             });
             setData(data);
             return;
         }
     };
+
+    useEffect(async () => {
+        const {data: {ip}} = await axios.get('http://ip.jsontest.com/');
+        const data = await getGeoIP({
+            ip,
+        });
+        setData(data);
+    }, []);
 
     return (
         <Tracker ip={data.ip}
